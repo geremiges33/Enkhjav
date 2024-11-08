2b55b1bdef2e84bf382a5c85a630609177dfa052
Your Name <you@example.com>
2024-11-08 14:18:01 +0800

    update

---
+++ enkhjav/pages/index.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return ( 
+    <div className="w-100% h-100% bg-color-black">
+      
+    <div
+      className="text-2xl">
+        <img className="w-64 h-64 rounded-full flex justify-center align-center" src="https://t.ly/m7BTV"/>
+        <h1 className="text-indigo-500"> Enkhjav</h1>
+    </div>
+    </div>
+  );
+}
